b0d4651624064bc0c196008a07491bcff83ea9dd
Duy Dat <duydat30122002@gmail.com>
2022-01-15 18:39:46 +0700

    Update js for header

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,13 @@ const menuClose = document.querySelector('.menu-close');
 const barWrapper = document.querySelector('.bar-wrapper');
 const navBar = document.querySelector('.nav');
 const navList = document.querySelector('.nav-list');
-const subnavList = document.querySelector('.subnav-list');
 const navTitles = document.querySelectorAll('.nav-title');
+const subnavList = document.querySelector('.subnav-list');
 const subnavTitles = document.querySelectorAll('.subnav-title');
+const searchBtn = document.querySelector('.search-btn');
+const searchBar = document.querySelector('.search-bar');
+const searchPanel = document.querySelector('.search-panel');
+
 
 /* ===== SHOW/HIDE HEADER ===== */
 var lastScroll = 0;
@@ -37,8 +41,8 @@ headerMenu.onclick = () => {
     menuClose.classList.toggle('hide');
     barWrapper.classList.toggle('active');
     navBar.classList.toggle('active');
-    overlay.classList.toggle('active');
-    body.classList.toggle('overlay-active');
+    overlay.classList.add('active');
+    body.classList.add('overlay-active');
     const panelActive = subnavList.querySelector('.has-panel.active');
     if (panelActive)
         panelActive.classList.remove('active');
@@ -76,7 +80,15 @@ subnavTitles.forEach(item => {
     }
 });
 
-
+/* ===== SHOW/HIDE SEARCH ===== */
+searchBtn.onclick = () => {
+    searchBtn.classList.toggle('active');
+    searchBar.classList.toggle('active');
+    searchPanel.classList.toggle('active');
+    overlay.classList.add('active');
+    body.classList.add('overlay-active');
+};
+    
 /* ===== CLICK OUTSIDE ===== */
 document.onclick = (e) => {
     if (!navBar.contains(e.target) && !headerMenu.contains(e.target)) {
@@ -85,13 +97,73 @@ document.onclick = (e) => {
         menuClose.classList.add('hide');
         barWrapper.classList.remove('active');
         navBar.classList.remove('active');
-        overlay.classList.remove('active');
-        body.classList.remove('overlay-active');
+
         const panelActive = subnavList.querySelector('.has-panel.active');
         if (panelActive)
             panelActive.classList.remove('active');
     }
-}
+    
+    if (!searchPanel.contains(e.target) && !searchBtn.contains(e.target)) {
+        searchPanel.classList.remove('active');
+        searchBtn.classList.remove('active');
+        searchBar.classList.remove('active');
+    }
+
+    if (!(headerMenu.className + " " + searchPanel.className).includes('active')) {
+        overlay.classList.remove('active');
+        body.classList.remove('overlay-active');
+    }
+};
+
+/* ===== PANEL SCROLL ===== */
+const panelScrolls = document.querySelectorAll('.header__panel-scroll');
+
+panelScrolls.forEach(panel => {
+    panel.style.maxHeight = `${window.innerHeight - 100}px`;
+});
+
+window.onresize = () => {
+    panelScrolls.forEach(panel => {
+        panel.style.maxHeight = `${window.innerHeight - 100}px`;
+    });
+};
+
+/* ===== MENU PANEL ===== */
+const menuPanelItems = document.querySelectorAll('.menu-panel-item');
+const menuPanelBg = document.querySelector('.header__panel-bg');
+
+menuPanelItems.forEach(item => {
+    item.onmouseover = () => {
+        item.classList.add('active');
+        overlay.classList.add('active');
+        body.classList.add('overlay-active');
+
+        headerMenu.classList.remove('active');
+        menuOpen.classList.remove('hide');
+        menuClose.classList.add('hide');
+        barWrapper.classList.remove('active');
+        navBar.classList.remove('active');
+        const panelActive = subnavList.querySelector('.has-panel.active');
+        if (panelActive)
+            panelActive.classList.remove('active');
+            
+        searchPanel.classList.remove('active');
+        searchBtn.classList.remove('active');
+        searchBar.classList.remove('active');
+
+        const itemHeight = item.querySelector('.header__panel').offsetHeight;
+        menuPanelBg.style.height = `${itemHeight}px`;
+    }
+
+    item.onmouseleave = () => {
+        item.classList.remove('active');
+        overlay.classList.remove('active');
+        body.classList.remove('overlay-active');
+        menuPanelBg.style.height = `0px`;
+    }
+});
+
+
 
 
 
